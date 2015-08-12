svn2github/valgrind-trunk

commit 2e6cbdf3ab9980355bafd7be85c4b358def4f001

Add support for repository cache protocol version 26. Patch contributed by Tomas Jedlicka <jedlickat@gmail.com>. n-i-bz
git-svn-id: svn://svn.valgrind.org/valgrind/trunk@15518 a5019735-40e9-0310-863c-91ae7b9d1cf9

coregrind/m_syswrap/syswrap-solaris.c
@@ -28,6 +28,8 @@
    The GNU General Public License is contained in the file COPYING.
 */
 
+/* Copyright 2015-2015, Tomas Jedlicka <jedlickat@gmail.com>. */
+
 /* Copyright 2013-2015, Ivo Raisr <ivosh@ivosh.net>. */
 
 /* Copyright 2013, OmniTI Computer Consulting, Inc. All rights reserved. */
@@ -7816,6 +7818,16 @@ static void repository_door_pre_mem_door_call_hook(ThreadId tid, Int fd,
                            "entity_name->rpr_entityid)", r->rpr_entityid);
             PRE_FIELD_READ("door_call(\"" VKI_REPOSITORY_DOOR_NAME "\", "
                            "entity_name->rpr_answertype)", r->rpr_answertype);
+         }
+         break;
+      case VKI_REP_PROTOCOL_ENTITY_GET_ROOT:
+         {
+            struct vki_rep_protocol_entity_root *r =
+               (struct vki_rep_protocol_entity_root *) p;
+            PRE_FIELD_READ("door_call(\"" VKI_REPOSITORY_DOOR_NAME "\", "
+                           "entity_root->rpr_entityid)", r->rpr_entityid);
+            PRE_FIELD_READ("door_call(\"" VKI_REPOSITORY_DOOR_NAME "\", "
+                           "entity_root->rpr_outid)", r->rpr_outid);
          }
          break;
       case VKI_REP_PROTOCOL_ENTITY_GET:

include/vki/vki-solaris-repcache.h
@@ -28,6 +28,8 @@
    The GNU General Public License is contained in the file COPYING.
 */
 
+/* Copyright 2015-2015, Tomas Jedlicka <jedlickat@gmail.com>. */
+
 #ifndef __VKI_SOLARIS_REPCACHE_H
 #define __VKI_SOLARIS_REPCACHE_H
 
@@ -175,6 +177,60 @@ enum vki_rep_protocol_requestid {
    VKI_REP_PROTOCOL_ENTITY_IS_MASKED,
    VKI_REP_PROTOCOL_CLIENT_REMOVE_NOTIFY,
 };
+#elif (SOLARIS_REPCACHE_PROTOCOL_VERSION == 26)
+#define VKI_REPOSITORY_DOOR_VERSION (26 + VKI_REPOSITORY_DOOR_BASEVER)
+enum vki_rep_protocol_requestid {
+   VKI_REP_PROTOCOL_CLOSE = ('C' << 8),
+   VKI_REP_PROTOCOL_ENTITY_SETUP,
+   VKI_REP_PROTOCOL_ENTITY_NAME,
+   VKI_REP_PROTOCOL_ENTITY_FMRI,
+   VKI_REP_PROTOCOL_ENTITY_PARENT_TYPE,
+   VKI_REP_PROTOCOL_ENTITY_GET_CHILD,
+   VKI_REP_PROTOCOL_ENTITY_GET_PARENT,
+   VKI_REP_PROTOCOL_ENTITY_GET_ROOT,
+   VKI_REP_PROTOCOL_ENTITY_GET,
+   VKI_REP_PROTOCOL_ENTITY_UPDATE,
+   VKI_REP_PROTOCOL_ENTITY_CREATE_CHILD,
+   VKI_REP_PROTOCOL_ENTITY_CREATE_PG,
+   VKI_REP_PROTOCOL_ENTITY_DELETE,
+   VKI_REP_PROTOCOL_ENTITY_UNDELETE,
+   VKI_REP_PROTOCOL_ENTITY_REMOVE,
+   VKI_REP_PROTOCOL_ENTITY_DELCUST,
+   VKI_REP_PROTOCOL_BUNDLE_REMOVE,
+   VKI_REP_PROTOCOL_ENTITY_RESET,
+   VKI_REP_PROTOCOL_ENTITY_TEARDOWN,
+   VKI_REP_PROTOCOL_ITER_SETUP,
+   VKI_REP_PROTOCOL_ITER_START,
+   VKI_REP_PROTOCOL_ITER_READ,
+   VKI_REP_PROTOCOL_ITER_READ_VALUE,
+   VKI_REP_PROTOCOL_ITER_RESET,
+   VKI_REP_PROTOCOL_ITER_TEARDOWN,
+   VKI_REP_PROTOCOL_NEXT_SNAPLEVEL,
+   VKI_REP_PROTOCOL_SNAPSHOT_TAKE,
+   VKI_REP_PROTOCOL_SNAPSHOT_TAKE_NAMED,
+   VKI_REP_PROTOCOL_SNAPSHOT_ATTACH,
+   VKI_REP_PROTOCOL_PROPERTY_GET_TYPE,
+   VKI_REP_PROTOCOL_PROPERTY_GET_VALUE,
+   VKI_REP_PROTOCOL_PG_SETUP_WAIT,
+   VKI_REP_PROTOCOL_PG_TX_START,
+   VKI_REP_PROTOCOL_PG_TX_COMMIT,
+   VKI_REP_PROTOCOL_PROP_BUNDLE_REMOVE,
+   VKI_REP_PROTOCOL_CLIENT_ADD_NOTIFY,
+   VKI_REP_PROTOCOL_CLIENT_WAIT,
+   VKI_REP_PROTOCOL_BACKUP,
+   VKI_REP_PROTOCOL_SET_AUDIT_ANNOTATION,
+   VKI_REP_PROTOCOL_UNSET_AUDIT_ANNOTATION,
+   VKI_REP_PROTOCOL_SET_TX_DECORATION,
+   VKI_REP_PROTOCOL_SWITCH,
+   VKI_REP_PROTOCOL_DECORATION_GET_LAYER,
+   VKI_REP_PROTOCOL_DECORATION_GET_TYPE,
+   VKI_REP_PROTOCOL_DECORATION_GET_VALUE,
+   VKI_REP_PROTOCOL_DECORATION_GET_BUNDLE,
+   VKI_REP_PROTOCOL_ENTITY_INCONFLICT,
+   VKI_REP_PROTOCOL_ENTITY_IS_MASKED,
+   VKI_REP_PROTOCOL_CLIENT_REMOVE_NOTIFY,
+   VKI_REP_PROTOCOL_MAX_REQUEST
+};
 #else
 #error Unsupported repcache protocol version
 #endif
@@ -226,6 +282,11 @@ struct vki_rep_protocol_entity_get_child {
    char rpr_name[VKI_REP_PROTOCOL_NAME_LEN];
 };
 struct vki_rep_protocol_entity_parent {
+   enum vki_rep_protocol_requestid rpr_request;
+   vki_uint32_t rpr_entityid;
+   vki_uint32_t rpr_outid;
+};
+struct vki_rep_protocol_entity_root {
    enum vki_rep_protocol_requestid rpr_request;
    vki_uint32_t rpr_entityid;
    vki_uint32_t rpr_outid;

commit 01349065e94888feb063e5cd1004b712ede66c85

Fix the regression from r15518 which broke builds on older Solaris releases.
git-svn-id: svn://svn.valgrind.org/valgrind/trunk@15530 a5019735-40e9-0310-863c-91ae7b9d1cf9

coregrind/m_syswrap/syswrap-solaris.c
@@ -7820,6 +7820,7 @@ static void repository_door_pre_mem_door_call_hook(ThreadId tid, Int fd,
                            "entity_name->rpr_answertype)", r->rpr_answertype);
          }
          break;
+      #if (SOLARIS_REPCACHE_PROTOCOL_VERSION >= 25)
       case VKI_REP_PROTOCOL_ENTITY_GET_ROOT:
          {
             struct vki_rep_protocol_entity_root *r =
@@ -7830,6 +7831,7 @@ static void repository_door_pre_mem_door_call_hook(ThreadId tid, Int fd,
                            "entity_root->rpr_outid)", r->rpr_outid);
          }
          break;
+      #endif /* SOLARIS_REPCACHE_PROTOCOL_VERSION >= 25 */
       case VKI_REP_PROTOCOL_ENTITY_GET:
          {
             struct vki_rep_protocol_entity_get *r =